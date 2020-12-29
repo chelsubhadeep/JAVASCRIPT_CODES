166a7a88fac01fb797a7bcd467215d09f675a9d8
subhadeep.chel <chelsubhadeep@gmail.com>
2020-12-29 11:25:59 +0530

    Header,Footer,Ninja Component are added

---
+++ VUEJS-YOUTUBE-TUTORIAL/L15_VUECLI/29-12-2020/Header/MyWebSite/recording/vuejs-playlist/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
+  </div> 
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default { 
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+  data () {
+    return {
+       
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: purple;
+}
+</style>
---
+++ VUEJS-YOUTUBE-TUTORIAL/L15_VUECLI/29-12-2020/Header/MyWebSite/recording/vuejs-playlist/src/components/Ninjas.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id = "ninjas">
+    <ul>
+      <li v-for="ninja in ninjas" v-on:click = "ninja.show = !ninja.show">
+       <h2>{{ ninja.name }}</h2>
+        <h3 v-show = "ninja.show">{{ ninja.specialty }}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default { 
+  data () {
+    return {
+      ninjas: [
+        {name: 'Ryu', specialty: 'Vue Components', show: false},
+        {name: 'Crystal', specialty: 'HTML Wizardry', show: false},
+        {name: 'Hitoshi', specialty: 'Click Events', show: false},
+        {name: 'Tango', specialty: 'Conditionals', show: false},
+        {name: 'Kami', specialty: 'Webpack', show: false},
+        {name: 'Yoshi', specialty: 'Data Diggin', show: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #ninjas{
+      width: 100%;
+      max-width: 1200px;
+      margin: 40px auto;
+      padding: 0 20px;
+      box-sizing: border-box;
+  }
+  ul{
+      display: flex;
+      flex-wrap: wrap;
+      list-style-type: none;
+      padding: 0;
+  }
+  li{
+      flex-grow: 1;
+      flex-basis: 300px;
+      text-align: center;
+      padding: 30px;
+      border: 1px solid #222;
+      margin: 10px;
+  }
+
+
+</style>
