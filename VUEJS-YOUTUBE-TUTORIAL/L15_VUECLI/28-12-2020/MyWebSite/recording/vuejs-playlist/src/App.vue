7c24272aa53ea1d16066ee3a4db50f3ea554efce
subhadeep.chel <chelsubhadeep@gmail.com>
2020-12-29 10:21:03 +0530

    VueCLI added

---
+++ VUEJS-YOUTUBE-TUTORIAL/L15_VUECLI/28-12-2020/MyWebSite/recording/vuejs-playlist/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default { 
+  components: {
+    'header': Header
+  }
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: purple;
+}
+</style>
