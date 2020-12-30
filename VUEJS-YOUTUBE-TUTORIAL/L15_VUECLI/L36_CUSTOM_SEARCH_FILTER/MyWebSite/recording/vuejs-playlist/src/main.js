575c190dc44afe4966a7ce6f139885193e1e49dd
subhadeep.chel <chelsubhadeep@gmail.com>
2020-12-30 14:18:38 +0530

    add CUSTOM SEARCH

---
+++ VUEJS-YOUTUBE-TUTORIAL/L15_VUECLI/L36_CUSTOM_SEARCH_FILTER/MyWebSite/recording/vuejs-playlist/src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+// import Ninjas from './Ninjas.vue'
+// Vue.component('ninjas', Ninjas);
+// export const bus = new Vue(); 
+
+Vue.use(VueResource);
+//custom directives
+Vue.directive('rainbow',{
+	bind(el,binding,vnode){
+		el.style.color = "#" + Math.random().toString().slice(2,8);
+	}
+});
+
+Vue.directive('theme',{
+	bind(el, binding, vnode){
+		if(binding.value == 'wide'){
+			el.style.maxWidth = "1200px";
+		}
+		else if(binding.value == 'narrow'){
+			el.style.maxWidth = "560px";
+		}
+		if(binding.arg == 'column'){
+			el.style.background = '#ddd';
+			el.style.padding = '20px';
+		}
+	}
+});
+
+// Filters
+Vue.filter('to-uppercase', function(value){
+	return value.toUpperCase();
+});
+
+Vue.filter('snippet', function(value){
+	return value.slice(0, 100) + '...';
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
