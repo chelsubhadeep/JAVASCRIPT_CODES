2aa74ae93e1861a6b0627f1dfc0f211a2cce35c7
subhadeep.chel <chelsubhadeep@gmail.com>
2020-12-30 14:43:25 +0530

    add global filter and directives

---
+++ VUEJS-YOUTUBE-TUTORIAL/L15_VUECLI/L37_GLOBAL_FILTER_DIRECTIVES/MyWebSite/recording/vuejs-playlist/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!--<add-blog></add-blog>-->
+    <show-blogs></show-blogs>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+
+export default { 
+  components:{
+    'add-blog': addBlog,
+    'show-blogs': showBlogs
+  },
+
+  data () {
+    return{
+
+    }
+   
+  },
+  methods:{
+  
+  }
+}
+</script>
+
+<style>
+body{
+  margin:0;
+  font-family:'Nunito SemiBold';
+}
+</style>
---
+++ VUEJS-YOUTUBE-TUTORIAL/L15_VUECLI/L37_GLOBAL_FILTER_DIRECTIVES/MyWebSite/recording/vuejs-playlist/src/components/showBlogs.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-theme:column="'narrow'" id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <input type="text" v-model="search" placeholder="search blogs"/>
+    <div v-for="blog in filteredBlogs" class="single-blog">
+      <h2 v-rainbow="">{{ blog.title | to-uppercase }}</h2>
+      <article>{{ blog.body | snippet}}</article>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default { 
+  data () {
+    return{
+      blogs: [],
+      search:''
+    }
+  },
+  methods:{
+  
+  },
+  created(){
+    this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+      this.blogs = data.body.slice(0, 10)
+    })
+  },
+  computed:{
+    filteredBlogs: function(){
+    return this.blogs.filter((blog) => {
+      return blog.title.match(this.search);
+    });
+    }
+  },
+  filters:{
+    toUppercase(value){
+        return value.toUpperCase();
+    }
+  },
+  directives:{
+    'rainbow': {
+      bind(el,binding,vnode){
+        el.style.color = "#" + Math.random().toString(16).slice(2,8);
+      }
+    }
+  }
+
+}
+
+</script>
+
+<style>
+#show-blogs{
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
