a2610b7f8e3b81d05fdbc391f57585f848cb719c
subhadeep.chel <chelsubhadeep@gmail.com>
2020-12-30 13:48:49 +0530

    add filters

---
+++ VUEJS-YOUTUBE-TUTORIAL/L15_VUECLI/L35_FILTERS/MyWebSite/recording/vuejs-playlist/src/components/showBlogs.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-theme:column="'narrow'" id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      <h2 v-rainbow="">{{ blog.title | to-uppercase }}</h2>
+      <article>{{ blog.body | snippet}}</article>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default { 
+  data () {
+    return{
+      blogs: []
+    }
+  },
+  methods:{
+  
+  },
+  created(){
+    this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+      this.blogs = data.body.slice(0, 10)
+    })
+  }
+}
+</script>
+
+<style>
+#show-blogs{
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
