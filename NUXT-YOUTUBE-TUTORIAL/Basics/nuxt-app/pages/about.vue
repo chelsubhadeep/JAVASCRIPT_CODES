5a32c229f6b0c7e4f263dad9efbdc090f54bbb4a
subhadeep.chel <chelsubhadeep@gmail.com>
2021-01-04 23:34:47 +0530

    Add Custom error and layout

---
+++ NUXT-YOUTUBE-TUTORIAL/Basics/nuxt-app/pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<nuxt-link to="/">Home</nuxt-link>
+		<h1>About Page</h1>
+	</div>
+</template>
+
+<script>
+
+export default{
+	
+}
+
+</script>
+
+<style>
+
+
+</style>
---
+++ NUXT-YOUTUBE-TUTORIAL/Basics/nuxt-app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<nuxt-link to="/about">About</nuxt-link>
+		<h1>Home Page</h1>
+	</div>
+
+</template>
+
+<script>
+
+export default{
+	
+}
+
+</script>
+
+<style>
+
+
+</style>
