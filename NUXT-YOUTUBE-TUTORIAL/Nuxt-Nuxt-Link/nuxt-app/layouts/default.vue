3ead0379bfb02a82afbdc19a4cee35043a85cf62
subhadeep.chel <chelsubhadeep@gmail.com>
2021-01-05 00:08:34 +0530

    Add meta tag and nuxt nuxt link

---
+++ NUXT-YOUTUBE-TUTORIAL/Nuxt-Nuxt-Link/nuxt-app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>NuxtJs Tutorial</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat><n-link to="/">Home</n-link></v-btn>
+        <v-btn flat><n-link to="/about">About</n-link></v-btn>
+        <v-btn flat><n-link to="/inspire">Inspire</n-link></v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <nuxt/>
+  </div>
+</template>
+
---
+++ NUXT-YOUTUBE-TUTORIAL/Nuxt-Nuxt-Link/nuxt-app/pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h1>{{title}}</h1>
+		<!--<router-link to="/">Home</router-link>-->
+		<!--<n-link to="/">Home</n-link>-->
+		<!--<nuxt-link to="/">Home</nuxt-link>-->
+		<NuxtLink to ="/">Home</NuxtLink>
+	</div>
+</template>
+
+<script>
+	export default{
+		asyncData(){
+			return { title: 'About'}
+		},
+		head(){
+			return{
+				title: 'About',
+				meta:[
+					{
+						hid: 'description',
+						name: 'description', 
+						content: 'My Amazing About Page'}
+				],
+				script: [{ src : '/about.js' }]
+			}
+		}
+	}
+</script>
+
