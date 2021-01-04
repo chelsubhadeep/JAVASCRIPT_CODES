7f3b51c484e439c93d14359e13724354274b4d58
subhadeep.chel <chelsubhadeep@gmail.com>
2021-01-04 22:29:39 +0530

    attributes of nuxt.js

---
+++ NUXT-YOUTUBE-TUTORIAL/Attributes/nuxt-app/layouts/error.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    This is error page
+    <h1>{{ error.statusCode }} not found</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+    layout: 'error-layout',
+    props: {
+      error: {
+        type: Object,
+        default: null
+      }
+    }
+  }
+</script>
+
+<!--<template>
+  <v-app dark>
+    <h1 v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred'
+    }
+  },
+  head () {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>-->
---
+++ NUXT-YOUTUBE-TUTORIAL/Attributes/nuxt-app/pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1>{{title}}</h1>
+	</div>
+</template>
+
+<script>
+	export default{
+		asyncData(){
+			return { title: 'About'}
+		},
+		head(){
+			return{
+				title: 'About',
+				meta:[
+					{name: 'description', content: 'My Amazing About Page'}
+				],
+				script: [{ src : '/about.js' }]
+			}
+		}
+	}
+</script>
+
